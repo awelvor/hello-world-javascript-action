412c780b4f9126668eda833aabdbdf0c3b0493ae
awelvor <philippe.regent@gmail.com>
2025-09-03 20:47:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 
-try {
+
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
   core.info(`Hello ${nameToGreet}!`);
@@ -10,9 +10,4 @@ try {
   const time = new Date().toTimeString();
   core.setOutput("time", time);
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  core.info(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+  
